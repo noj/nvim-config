7e880d348acedd082ced3e3a99f14e1a4d6b92d2
Jon Olsson <jon.olsson@gmail.com>
2025-05-22 08:35:02 +0200

    Sync latest config

---
+++ lua/jon/lazy/cpp.lua
@@ -0,0 +1,3 @@
+return {
+    'bfrg/vim-c-cpp-modern'
+}
--- lua/jon/remap.lua
+++ lua/jon/remap.lua
@@ -24,6 +24,8 @@ vim.keymap.set("n", "<leader>A", ":ClangdSwitchSourceHeader<CR>")
 vim.keymap.set("n", "]g", vim.diagnostic.goto_next)
 vim.keymap.set("n", "[g", vim.diagnostic.goto_prev)
 
+vim.keymap.set("n", "ga", vim.lsp.buf.code_action)
+
 -- LSP
 -- vim.keymap.set("n", "gr", "", {
 --   callback = require("telescope.builtin").lsp_references
@@ -34,6 +36,19 @@ vim.keymap.set("n", "[g", vim.diagnostic.goto_prev)
 vim.keymap.set('n', '<leader>td', function()
   vim.diagnostic.enable(not vim.diagnostic.is_enabled())
 end, { silent = true, noremap = true })
+
+-- TreeWalker
+-- movement
+vim.keymap.set({ 'n', 'v' }, '<C-k>', '<cmd>Treewalker Up<cr>', { silent = true })
+vim.keymap.set({ 'n', 'v' }, '<C-j>', '<cmd>Treewalker Down<cr>', { silent = true })
+vim.keymap.set({ 'n', 'v' }, '<C-h>', '<cmd>Treewalker Left<cr>', { silent = true })
+vim.keymap.set({ 'n', 'v' }, '<C-l>', '<cmd>Treewalker Right<cr>', { silent = true })
+
+-- swapping
+vim.keymap.set('n', '<C-S-k>', '<cmd>Treewalker SwapUp<cr>', { silent = true })
+vim.keymap.set('n', '<C-S-j>', '<cmd>Treewalker SwapDown<cr>', { silent = true })
+vim.keymap.set('n', '<C-S-h>', '<cmd>Treewalker SwapLeft<cr>', { silent = true })
+vim.keymap.set('n', '<C-S-l>', '<cmd>Treewalker SwapRight<cr>', { silent = true })
 
 -- Quick current-word-under-cursor highlighting toggle, toggle via space bar
 vim.keymap.set("n", " ", "", {
